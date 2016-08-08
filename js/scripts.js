38acbcd6ae02bf73f0286ee3929b564f2e48c24f
Megan Kaluza and Satchel Grant <megan@>
2016-08-08 13:16:40 -0700

    added new functions to our calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,35 @@
 var add = function(number1, number2) {
 	return number1 + number2;
-}
-
+};
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+var remainder = function(number1, number2) {
+  return number1 % number2;
+};
+var BMI = function(weight, height) {
+	return weight / height;
+};
+var tempC2F = function(celsius) {
+	return celsius * 9 / 5 + 32 ;
+};
+var tempF2C = function(fahrenheit) {
+	return (fahrenheit - 32) * 5 / 9;
+};
+var converterG2L = function(gallons) {
+	return gallons * 3.78541;
+};
+var converterL2G = function(liters) {
+	return liters / 3.78541;
+};
 var num1 = parseInt(prompt("Enter a number: "));
 var num2 = parseInt(prompt("Enter another number: "));
 
-alert(add(num1,num2));
+var result = add(num1,num2);
+alert(result);
